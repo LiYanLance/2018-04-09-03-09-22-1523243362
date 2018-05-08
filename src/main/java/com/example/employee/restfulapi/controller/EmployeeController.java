e3cb60faeac9b83ae2e3527a57d672e4ab8fbd51
LiYanLance <leeyanly@163.com>
2018-05-08 17:37:40 +0800

    feat complete GET /employees/1

--- src/main/java/com/example/employee/restfulapi/controller/EmployeeController.java
+++ src/main/java/com/example/employee/restfulapi/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import com.example.employee.restfulapi.entity.Employee;
 import com.example.employee.restfulapi.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,10 @@ public class EmployeeController {
     @GetMapping
     public List<Employee> getEmployees() {
         return employeeRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Employee getEmployee(@PathVariable long id) {
+        return employeeRepository.findOne(id);
     }
 }
