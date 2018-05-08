588e3a41a69e5253026b87729ed45b8741fd9285
LiYanLance <leeyanly@163.com>
2018-05-08 17:42:32 +0800

    feat complete GET /employees/page/1/pageSize/2

--- src/main/java/com/example/employee/restfulapi/controller/EmployeeController.java
+++ src/main/java/com/example/employee/restfulapi/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.example.employee.restfulapi.controller;
 import com.example.employee.restfulapi.entity.Employee;
 import com.example.employee.restfulapi.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,5 +26,10 @@ public class EmployeeController {
     @GetMapping("/{id}")
     public Employee getEmployee(@PathVariable long id) {
         return employeeRepository.findOne(id);
+    }
+
+    @GetMapping("/page/{page}/pageSize/{pageSize}")
+    public List<Employee> getEmployeesByPage(@PathVariable int page, @PathVariable int pageSize) {
+        return employeeRepository.findAll(new PageRequest(page - 1, pageSize)).getContent();
     }
 }
